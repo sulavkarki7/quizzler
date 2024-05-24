e7089101a21194dff05cdba39718ff95aafd76e7
Sulav Karki <sulavkarki08@gmail.com>
2024-05-24 19:55:51 +0545

    commiting

--- lib/main.dart
+++ lib/main.dart
@@ -67,19 +67,8 @@ class _QuizPageState extends State<QuizPage> {
 
   List<Icon> scoreKeeper = [];
 
-  // List<String> questions = [
-  //   'You can lead a cow down stairs but not up stairs',
-  //   'Approximately one quarter of human bones are in the feet ',
-  //   'A slug\'s blood is green'
-  // ];
   int questionNumber = 0;
-  // List<bool> answers = [
-  //   false,
-  //   true,
-  //   true,
-  // ];
 
-  // List<Question> questionBank = [
   //    Question q1 = Question(q:'You can lead a cow down stairs but not up stairs',a: false),
   // Question q2 = Question(q:'Approximately one quarter of human bones are in the feet ',a: true ),
   // Question q3 = Question(q: 'A slug\'s blood is green',a: true);
